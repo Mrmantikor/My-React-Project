e4869547776021814d23a3d8b405175946c07610
Bohdan Vasylovych <war.mant1kor@gmail.com>
2025-05-23 20:15:14 +0300

    react

--- src/pages/Todos/Todos.jsx
+++ src/pages/Todos/Todos.jsx
@@ -1,7 +1,49 @@
 import React from "./Todos.module.scss";
 
 const Todos = () => {
-  return <div>Todos</div>;
+  return (
+    <div className={s.welcomePage}>
+      <Media query="(max-width: 1439px)">
+        {(matches) => matches && <BgImageWrapper />}
+      </Media>
+      <div className={s.welcomePage__home}>
+        <div className={s.welcomePage__text}>
+          <p className={s.welcomePage__subtitle}>Expense log</p>
+          <h1 className={s.welcomePage__title}>
+            Manage Your{" "}
+            <span className={s.welcomePage__titleHighlight}>Finances</span>{" "}
+            Masterfully!
+          </h1>
+          <p className={s.welcomePage__description}>
+            ExpenseTracker effortlessly empowers you to take control of your
+            finances! With intuitive features, it simplifies the process of
+            tracking and managing expenses, allowing for a stress-free mastery
+            over your financial world.
+          </p>
+          <div className={s.welcomePage__buttons}>
+            <NavLink to="/register" className={s.welcomePage__button_signup}>
+              Sign Up
+            </NavLink>
+            <NavLink to="/login" className={s.welcomePage__button_signin}>
+              Sign In
+            </NavLink>
+          </div>
+        </div>
+        <div className={s.welcomePage__image}>
+          <picture>
+            <source srcSet={`${users}, ${users2x} 2x`} type="image/avif" />
+            <img className={s.welcomePage__imageMain} src={users} alt="Users" />
+          </picture>
+          <div className={s.welcomePage__users}>
+            <h2 className={s.welcomePage__usersTitle}>1000 users +</h2>
+            <p className={s.welcomePage__usersText}>
+              Trusted by users for reliable expense tracking!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Todos;
